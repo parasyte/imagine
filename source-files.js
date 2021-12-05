367350f63a849db1dbe1438036084c2b4f8aca65
Lokathor <zefria@gmail.com>
2021-12-05 17:17:28 +0000

    Deploying to gh-pages from  @ 1a7beb5ab56de50cd9de31a6fa8f7208a221c63b 🚀

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["adler"] = {"name":"","files":["algo.rs","lib.rs"]};
 sourcesIndex["bytemuck"] = {"name":"","files":["contiguous.rs","lib.rs","offset_of.rs","pod.rs","transparent.rs","zeroable.rs"]};
-sourcesIndex["imagine"] = {"name":"","dirs":[{"name":"png","files":["chunks.rs","mod.rs"]}],"files":["lib.rs","pixel_formats.rs"]};
+sourcesIndex["imagine"] = {"name":"","dirs":[{"name":"png","files":["chunks.rs","mod.rs","unfilter.rs"]}],"files":["lib.rs","pixel_formats.rs"]};
 sourcesIndex["miniz_oxide"] = {"name":"","dirs":[{"name":"deflate","files":["buffer.rs","core.rs","mod.rs","stream.rs"]},{"name":"inflate","files":["core.rs","mod.rs","output_buffer.rs","stream.rs"]}],"files":["lib.rs","shared.rs"]};
 createSourceSidebar();
